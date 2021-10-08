8cddec2b08d5afa91a9fb0b7cb6389c61456177e
joaoorlando <jel.phd@gmail.com>
2021-10-08 12:48:40 -0300

    Atualizado para versão correta.

--- 1940.py
+++ 1940.py
@@ -1,31 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[12]:
-
-
-# URI 1715
-
-n, m = map(int, input().split())
-val = [all(map(int, input().split())) for _ in range(n)]
-
-print(sum(val))
-
-
-# In[19]:
-
-
-# URI 1940
-
 jog, rod = map(int, input().split())
 
 pontos = list(map(int, input().split()))
 soma_pontos = [sum([pontos[j] for j in range(i, jog*rod, jog)]) for i in range(jog)]
 soma_pontos = soma_pontos[::-1]
 print(len(soma_pontos)-soma_pontos.index(max(soma_pontos)))
-
-
-# In[ ]:
 
 
 
